fabacf657c618589030413f8e6994428c8417317
NazarAvilin <avilin1@outlook.com>
2020-05-06 12:20:53 +0500

    6. Идеальный вес.[#282110]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,21 +2,23 @@ package ru.job4j.calculator;
 
 public class Fit {
 
+    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static double manWeight(Double height){
-        double ideal = height - 100;
+        double ideal = ((height - 100) * 1.15d);
         return ideal;
     }
 
     public static double womanWeight(Double height){
-        double ideal = height - 110;
+        double ideal = ((height - 110) * 1.15d);
         return ideal;
     }
 
     public static void main(String[] args) {
         double manWeight = Fit.manWeight(180d);
         double womanWeight = Fit.manWeight(165d);
-        System.out.println("Идеальный вес мужчины с ростом 180см = "+
-                manWeight + " \nИдеальный вес женщины с ростом 165см = "+womanWeight);
+        System.out.println("Идеальный вес мужчины с ростом 180см = " +
+                            manWeight + " \nИдеальный вес женщины с " +
+                            "ростом 165см = " + womanWeight);
     }
 
 }
