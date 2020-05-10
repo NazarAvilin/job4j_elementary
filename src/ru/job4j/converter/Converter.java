366ba2d312ff29b44abddcf404e77395c39ca05d
NazarAvilin <avilin1@outlook.com>
2020-05-10 17:17:01 +0500

    5.8. JavaDoc. Code convention[#282170]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,33 +1,38 @@
 package ru.job4j.converter;
-
+/**
+ * Класс для конвертации валюты.  Из рублей в доллары и евро.
+ *
+ * @author NazarAvilin
+ */
 public class Converter {
+    /**
+     * Метод перевода рублей в евро.
+     * @param value - кол-во рублей.
+     * @return rs1 - кол-во евро.
+     */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
+    /**
+     * Метод перевода рублей в доллары.
+     * @param value - кол-во рублей.
+     * @return rs1 - кол-во долларов.
+     */
     public static int rubleToDollar(int value) {
         int rsl = value / 60;
         return rsl;
     }
 
+    /**
+     * Main
+     * @param args - args.
+     */
     public static void main(String[] args) {
-/*
-        int in = 140;
-        int expected = 2;
-        int out = rubleToEuro(in);
-        boolean passed = expected == out;
-        System.out.println("140 rubles are 2 Euros. Test result : " + passed);
 
-        in = 120;
-        expected = 2;
-        out = rubleToDollar(in);
-        passed = expected == out;
-        System.out.println("120 rubles are 2 Dollars. Test result : " + passed);
-*/
         int rubleToEuro = Converter.rubleToEuro(70);
         int rubleToDollar = Converter.rubleToDollar(60);
         System.out.println("70 Rubles = " + rubleToEuro+" Еuro\n60 Rubles = "+rubleToDollar+" $");
-
 
     }
 }
