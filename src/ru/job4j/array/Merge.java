22f0361cf994985307cebcc79e017a0b8c2755df
NazarAvilin <avilin1@outlook.com>
2020-05-19 13:20:10 +0500

    Объединить два массива [#124482]

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Merge {
+    public int[] merge(int[] left, int[] right) {
+
+        int[] rsl = new int[left.length + right.length];
+
+        for (int i = 0; i < left.length; i++){
+            rsl[i] = left[i];
+        }
+        for (int i = 0; i < right.length; i++){
+            rsl[left.length + i] = right[i];
+        }
+
+
+        return rsl;
+    }
+
+}
