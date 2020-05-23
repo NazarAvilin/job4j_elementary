95f4d820d8826f93de4ac89819a0ed1e403fda21
NazarAvilin <avilin1@outlook.com>
2020-05-23 11:46:37 +0500

    New Update

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -4,7 +4,7 @@ public class MultiMax {
 
     public static int max(int first, int second, int third) {
 
-        int result = first > second && first > third? first : second;
+        int result = first > second && first > third ? first : second;
         result = third > result ? third : result;
 
         return result;
