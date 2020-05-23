39a4453e7c0e54b1d3c42ccf43aaa9ef1ec0af3e
NazarAvilin <avilin1@outlook.com>
2020-05-23 17:42:30 +0500

    2. Перегрузка конструктора. [#282263]

--- src/ru/job4j/oop/Animal.java
+++ src/ru/job4j/oop/Animal.java
@@ -1,10 +1,17 @@
 package ru.job4j.oop;
 
 public class Animal {
+    private String name;
+
     public Animal() {
         super();
-        System.out.println("Animal");
+        System.out.println(name);
     }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
 
     public static void main(String[] args) {
         Animal animal = new Animal();
