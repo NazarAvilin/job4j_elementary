01ba048fdba7b8af243638af1beb03f762ec0ff4
NazarAvilin <avilin1@outlook.com>
2020-05-26 19:42:16 +0500

    3. Интерфейс Input [#282210]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,13 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public String bus(String model) {
+        return null;
+    }
+
+    @Override
+    public int quantity(int setQuantity) {
+        return 0;
+    }
+}
---
+++ src/ru/job4j/poly/Transport.java
@@ -0,0 +1,6 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    String bus(String model);
+    int quantity(int setQuantity);
+}
