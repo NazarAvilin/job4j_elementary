f03e3630622b886e10c0e79db0823da783245b07
NazarAvilin <avilin1@outlook.com>
2020-05-27 23:03:40 +0500

    7. Шаблон проектирования - Стратегия. [#282221]

---
+++ src/ru/job4j/stragery/Paint.java
@@ -0,0 +1,11 @@
+package ru.job4j.stragery;
+
+public class Paint {
+    public void draw(Shape shape) {
+        System.out.print(shape.draw());
+    }
+
+    public static void main(String[] args) {
+        new Paint().draw(new Triangle());
+    }
+}
