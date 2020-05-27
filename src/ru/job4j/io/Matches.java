516e379805d7fc462a454edc84f1eaf3d6c06f56
NazarAvilin <avilin1@outlook.com>
2020-05-27 13:22:26 +0500

    2. Scanner и чтение числа из консоли.[179518#282209]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -6,25 +6,27 @@ public class Matches {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean run = true;
         int count = 11;
-        System.out.println("Ход первого игрока. Возьмите спички от 1 до 3 штук");
-        while (run) {
-            int select = Integer.parseInt(input.nextLine());
-            if (select > 0 && select < 4) {
-                count = count - select;
-                if (count <= 0) {
-                    System.out.println("Игра завершена. Последний кто взял, тот и выиграл! Поздравляем");
-                    run = false;
-                } else {
-                    System.out.println("На столе осталось:" + count + " спичек");
-                    System.out.println("Ход второго игрока. Возьмите спички от 1 до 3 штук");
-                }
+        boolean select = true;
+        String player;
+        while (count > 0) {
+            player = select ? "Первый" : "Второй";
+            System.out.print(player + " игрок берет спички ");
+            int size = Integer.parseInt(input.nextLine());
+            if (size > 3 || size < 0) {
+                System.out.println("Нужно взять от 1 до 3 спичек. Повторите попытку...");
             } else {
-                System.out.println("Нужно взять от 1 до 3 спичек. Игра звершена. Начните сначала");
-                run = false;
+                count -= size;
+                if (count < 0) {
+                    System.out.println("Нужно взять не больше чем то что осталось! Игра завершена.");
+                } else {
+                    System.out.println("Осталось " + count + " cпичек");
+                    select = !select;
+                }
             }
         }
+        player = select ? "Второй игрок выиграл! Поздравляем" : "Первый игрок выиграл! Поздравляем";
+        System.out.println(player);
     }
 
 }
