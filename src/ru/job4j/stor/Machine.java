32bd9ef87c9d4dd23f47c48a36a7536c0cfd94a8
NazarAvilin <avilin1@outlook.com>
2020-06-30 18:42:21 +0500

    1. Контрольные вопросы. [#282279], Сдача в кофе машине

--- src/ru/job4j/stor/Machine.java
+++ src/ru/job4j/stor/Machine.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class Machine {
 
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int result = money - price;
-        for (int coin : COINS) {
+        for (int coin : coins) {
             while (result >= coin) {
                 result = result - coin;
                 rsl[size] = coin;
